580de17353a537096de735a48f1ea7dd0b1ec511
Alex Skov Jensen <x821938@offline.dk>
2019-06-09 22:36:19 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Flogger for Node-Red
+
+This Node-Red module makes logging much easier and more granulated than the debug window of Node-Red. Please install it and see the help in the info-pane of Node-Red.
---
+++ flogger.js
@@ -0,0 +1,58 @@
+const fs = require('fs')
+
+module.exports = function(RED) {
+	function FloggerNode(n) {
+		RED.nodes.createNode(this,n);
+
+		this.loglevel = n.loglevel;
+		this.logfile = n.logfile;
+		this.appendtext = n.appendtext;
+		this.inputobject = n.inputobject;
+		this.logconfig = RED.nodes.getNode(n.logconfig);
+		var node = this;
+
+		node.on('input', function(msg) {
+			now = new Date();
+
+			path = node.logconfig.logdir + "/" + node.logfile;
+			loglevel = node.loglevel;
+			appendtext = node.appendtext || "";
+
+			if (node.logconfig.stamp) {
+				logstamp = now.toISOString() + " ";
+			} else {
+				logstamp = "";
+			};
+
+			if (node.inputobject) {
+				if (typeof(msg[node.inputobject]) == "object") {
+					logline = logstamp + " " + loglevel + "  " + appendtext + JSON.stringify(eval("msg." + node.inputobject)) + "\n";	
+				} else {
+					logline = logstamp + " " + loglevel + "  " + appendtext + eval("msg." + node.inputobject) + "\n";
+				}
+			} else {
+				logline = logstamp + " " + loglevel + "  " + appendtext + JSON.stringify(msg) + "\n";
+			};
+
+			fs.appendFile(path, logline, (err) => {  
+    			if (err) {
+    				node.status({shape: "ring", fill: "red", text: "Cant write file!"});
+    			} else {
+					nowstrstatus = now.toLocaleString();
+    				node.status({shape: "ring", fill: "green", text: nowstrstatus});
+    			}
+			});
+		});
+	}
+
+	RED.nodes.registerType("flogger",FloggerNode);
+
+    function FloggerConfigNode(n) {
+        RED.nodes.createNode(this,n);
+        this.logdir = n.logdir;
+        this.logname = n.logname;
+        this.stamp = n.stamp;
+    }
+
+ 	RED.nodes.registerType("config-log",FloggerConfigNode);
+}
